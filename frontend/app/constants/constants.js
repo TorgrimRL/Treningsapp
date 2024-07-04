49502ec9f93ca6789e39bcddcd12f8c0c63d0c66
TorgrimRL <tli080@uit.no>
2024-07-04 22:05:11 +0200

    Added constants for exercises and muscle groups

--- frontend/app/constants/constants.js
+++ frontend/app/constants/constants.js
@@ -20,3 +20,67 @@ export const exerciseTemplate = {
     videoLink:"",
     muscleGroup: muscleGroups[0],
 };
+
+export const exercises = {
+  "Abs": [
+    "Machine Crunch", "Slant Board Sit-Up", "Reaching Sit-Up", "V-Up", "Modified Candlestick", 
+    "Hanging Knee Raise", "Hanging Straight Leg Raise"
+  ],
+  "Calves": [
+    "Calves on Calf Machine", "Stair Calves", "Calves on Leg Press", "Smith Machine Calves"
+  ],
+  "Biceps": [
+    "Barbell Curl", "EZ Curl", "Close Grip Barbell Curl", "2-Arm Dumbbell Curl", "Cable Curl", 
+    "Incline Dumbbell Curl", "Dumbbell Twist Curl", "Hammer Curl", "Dumbbell Spider Curl", 
+    "Alternating Dumbbell Curl", "Cable Rope Twist Curl"
+  ],
+  "Rear Delts": [
+    "Barbell Facepull", "Dumbbell Facepull", "Cable Facepull", "Dumbbell Rear Lateral Raise"
+  ],
+  "Side Delts": [
+    "Barbell Upright Row", "Dumbbell Upright Row", "Cable Upright Row", "Dumbbell Side Lateral Raise", 
+    "Thumbs Down Lateral Raise"
+  ],
+   "Front Delts": 
+   ["Standing Barbell Shoulder Press", "Seated Barbell Shoulder Press", 
+    "Seated Dumbbell Shoulder Press", "High Incline Dumbbell Press", "Shoulder Press Machine", 
+    "Standing Dumbbell Shoulder Press"],
+  "Traps": [
+    "Barbell Shrug", "Barbell Bent Over Shrug", "Dumbbell Shrug", "Dumbbell Bent Over Shrug"
+  ],
+  "Triceps": [
+    "Skullcrusher", "JM Press", "Dips", "Assisted Dips", "Dumbbell Skullcrusher", "Cable Tricep Pushdown", 
+    "Cable Rope Pushdown", "Bar Skull", "EZ Bar Overhead Tricep Extension", "Barbell Overhead Tricep Extension", 
+    "Seated EZ Bar Overhead Tricep Extension", "Seated Barbell Overhead Tricep Extension", "Cable Overhead Tricep Extension", 
+    "Medium Grip Bench Press", "Close Grip Bench Press", "Close Grip Pushup", "Incline Close Grip Bench Press", 
+    "Skullcrusher", "EZ Bar Overhead Tricep Extension", "JM Press", "Barbell Overhead Tricep Extension", 
+    "Dips", "Seated EZ Bar Overhead Tricep Extension", "Assisted Dips", "Seated Barbell Overhead Tricep Extension", 
+    "Dumbbell Skullcrusher", "Cable Overhead Tricep Extension", "Cable Tricep Pushdown", "Cable Rope Pushdown", "Bar Skull"
+  ],
+  "Back": [
+    "Barbell Bent Over Row", "Underhand EZ Bar Row", "Row to Chest", "1-Arm Dumbbell Row", "Chest Supported Row", 
+    "Row Machine", "2-Arm Dumbbell Row", "Cable Row", "Overhand Pullup", "Parallel Pullup", "Underhand Pullup", 
+    "Wide Grip Pullup", "Assisted Overhand Pullup", "Assisted Parallel Pullup", "Assisted Underhand Pullup", 
+    "Normal Grip Pulldown", "Parallel Pulldown", "Underhand Pulldown", "Wide-Grip Pulldown", "Narrow Grip Pulldown"
+  ],
+  "Quads": [
+    "High Bar Squat", "Close Stance Feet Forward Squats", "Machine Feet Forward Squat", "Leg Press", "Hack Squat", 
+    "Front Squat", "Front Squat (Alternate Grip)"
+  ],
+  "Glutes": [
+    "Barbell Walking Lunge", "Dumbbell Walking Lunge", "Sumo Squat", "Deficit Deadlift", "25's Deadlift", 
+    "Sumo Deadlift", "Deadlift", "Hex Bar Deadlift"
+  ],
+  "Hamstrings": [
+    "Stiff-Legged Deadlift", "Low Bar Good Morning", "High Bar Good Morning", "45 Degree Back Raise", 
+    "Lying Leg Curl", "Seated Leg Curl", "Single-Leg Leg Curl"
+  ],
+  "Chest": [
+    "Flat Dumbbell Flye", "Incline Dumbbell Flye", "Cable Flye", "High Cable Flye", "Machine Chest Flye", 
+    "Cable Incline Flye", "Pec Dec Flye", "Medium Grip Bench Press", "Wide Grip Bench Press", 
+    "Flat Dumbbell Bench Press", "Flat Machine Bench Press", "Pushup", "Incline Medium Grip Bench Press", 
+    "Incline Wide Grip Bench Press", "Low Incline Dumbbell Press", "Incline Dumbbell Press", 
+    "Incline Machine Bench Press"
+  ]
+  
+};
