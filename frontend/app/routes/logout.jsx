1d1191021bd797e5fc480ca331ef8c40dd3ce311
TorgrimRL <tli080@uit.no>
2024-08-13 17:56:40 +0200

    fix: reroute from logout comp to home

--- frontend/app/routes/logout.jsx
+++ frontend/app/routes/logout.jsx
@@ -2,7 +2,7 @@ import { redirect } from "@remix-run/node";
 import Logout from "../components/Logout";
 
 export const action = async () => {
-  return redirect("/login", {
+  return redirect("/", {
     headers: {
       "Set-Cookie": "token=; Path=/; Max-Age=0; HttpOnly",
     },
