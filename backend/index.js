d9097c4a931f02128d48ca1ae0a6098a6cfff0a2
TorgrimRL <tli080@uit.no>
2024-08-13 15:25:37 +0200

    fix: Changed cookies sameSite from strict to none

--- backend/index.js
+++ backend/index.js
@@ -100,7 +100,7 @@ app.post("/api/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
     });
 
     res.json({ token });
