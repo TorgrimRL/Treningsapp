0e3d2cfd9cbcea795a29c2acefd2b8a1a06d79ec
Torgrim Limstrand <torgrimlimstrand@hotmail.com>
2026-08-16 21:20:40 +0200

    feat: integrate Tailwind CSS with Vite and update configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 import { defineConfig } from "vite";
 
@@ -12,7 +13,7 @@ export default defineConfig({
     ],
   },
 
-  plugins: [reactRouter()],
+  plugins: [tailwindcss(), reactRouter()],
 
   build: {
     sourcemap: false,
